c53e3d7029c31dfd72b25d653c722f10565d43bb
jaithunshifaya <jaithunshifaya@gmail.com>
2025-05-21 22:53:06 +0530

    Palindrome commit

---
+++ PalindromeCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int original = num, reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        if (original == reversed)
+            System.out.println("It's a palindrome.");
+        else
+            System.out.println("Not a palindrome.");
+
+        scanner.close();
+    }
+}
