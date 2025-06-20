3eaa1e7ed4e5f25047ff6a08d1f44118e0873b88
Jaithun Shifaya <151420651+jaithunshifaya@users.noreply.github.com>
2025-06-20 20:25:38 +0530

    Update AnagramCheck.java

--- AnagramCheck.java
+++ AnagramCheck.java
@@ -1,18 +1,28 @@
-import java.util.Arrays;
 public class AnagramCheck {
     public static void main(String[] args) {
         String str1 = "listen";
         String str2 = "silent";
 
-        char[] a = str1.toCharArray();
-        char[] b = str2.toCharArray();
-
-        Arrays.sort(a);
-        Arrays.sort(b);
-
-        if (Arrays.equals(a, b))
+        if (isAnagram(str1,str2){
             System.out.println("Anagram");
-        else
+        } else{
             System.out.println("Not Anagram");
+        }    
     }
+    public static boolean isAnagram(String str1, String str2){
+        if(str1.length()!=str2.length()){
+            return false;
+        }
+        int[] count=new int[26];
+        for(int i=0; i<str1.length(); i++){
+            count[str1.charAt(i) - 'a']++;
+            count[str2.charAt(i) - 'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }   
 }
