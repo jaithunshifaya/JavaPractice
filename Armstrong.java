bbf4a1b08fa9806e96c33ef1f46b181c548af3bf
Jaithun Shifaya <151420651+jaithunshifaya@users.noreply.github.com>
2025-06-20 20:30:42 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,16 +1,21 @@
 public class Armstrong {
     public static void main(String[] args) {
-        int num = 153, original = num, sum = 0;
+        int num = 153;
+         if (isArmstrong(num))
+            System.out.println(num + " is an Armstrong number");
+        else
+            System.out.println(num + " is not an Armstrong number");
+    }
+    public static boolean isArmstrong(int num){
+        int original=num;
+        int sum=0;
 
         while (num > 0) {
             int digit = num % 10;
             sum += digit * digit * digit;
             num /= 10;
         }
+        return sum==original;
 
-        if (sum == original)
-            System.out.println(original + " is an Armstrong number");
-        else
-            System.out.println(original + " is not an Armstrong number");
     }
 }
