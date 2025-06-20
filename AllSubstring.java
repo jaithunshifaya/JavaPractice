c5f62477109d8d72aef8c903e3e8623d9a725faf
Jaithun Shifaya <151420651+jaithunshifaya@users.noreply.github.com>
2025-06-20 20:18:51 +0530

    Update AllSubstring.java

--- AllSubstring.java
+++ AllSubstring.java
@@ -1,12 +1,11 @@
 public class AllSubstring{
-    public static void main(String[] args) {
-        String str = "Shifaya";
-        int len = str.length();
-
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j <= len; j++) {
-                System.out.println(str.substring(i, j));
+    public static void main(String[] args){
+        String str="Shifaya";
+        int len=str.length();
+        for(int i=0; i<len; i++){
+            for(int j=i+1; j<=len; j++){
+                System.out.println(str.substring(i,j));
             }
         }
-    }
+    }    
 }
