13c3640ee91df0abd66a765157c0a8a54e8df592
Jaithun Shifaya <151420651+jaithunshifaya@users.noreply.github.com>
2025-06-20 20:14:49 +0530

    Update AddTwoDate.java

--- AddTwoDate.java
+++ AddTwoDate.java
@@ -1,15 +1,12 @@
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-public class AddTwoDate {
-    public static void main(String[] args) {
-        String dateStr = "31-12-2025";
-        int daysToAdd = 3000;
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        LocalDate date = LocalDate.parse(dateStr, formatter);
-        LocalDate newDate = date.plusDays(daysToAdd);
-
-        System.out.println(newDate.format(formatter));
+public class AddTwoDate{
+    public static void main(String[] args){
+        String dateStr="29-12-2003";
+        int daysToAdd=273;
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd-MM-yyyy);
+        LocalDate date=LocalDate.parse(dateStr,formatter);
+        LocalDate newDate=date.plusDays(daysToAdd);
+        System.out.println(newDtae.format(formatter));
     }
-}
+}   
+    
