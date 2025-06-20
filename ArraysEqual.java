d63997216379e17669d10ab44f79eff05e71cdcd
Jaithun Shifaya <151420651+jaithunshifaya@users.noreply.github.com>
2025-06-20 20:35:30 +0530

    Update ArraysEqual.java

--- ArraysEqual.java
+++ ArraysEqual.java
@@ -1,14 +1,20 @@
-import java.util.Arrays;
-
 public class ArraysEqual {
     public static void main(String[] args) {
         int[] a = {1, 2, 3};
         int[] b = {3, 1, 2};
-
-        Arrays.sort(a);
-        Arrays.sort(b);
-
-        boolean isEqual = Arrays.equals(a, b);
+        boolean isEqual =areArraysEqual(a, b);
         System.out.println("Arrays are equal: " + isEqual);
     }
+    public static boolean areArraysEqual(int[] a, int[] b){
+        if(a.lenght != b.length) return false;
+        int[] freq=new int[1001];
+        for(int i=0; i<a.length; i++){
+            freq[a[i]]++; }
+        for(int i=0; i<b.length; i++){
+            freq[b[i]]--; }
+        for(int i=0; i<freq.length; i++){
+            if(freq[i]!=0) return false;
+        }
+        return true;
+    }   
 }
